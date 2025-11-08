636fdd159a7350ba28cf37eb20b39d9052973b28
Luan Gjokaj <luan@riangle.com>
2025-11-08 13:44:15 +0100

    feat: improve styles

--- components/layout/ActionBar.tsx
+++ components/layout/ActionBar.tsx
@@ -146,7 +146,11 @@ const StyledContent = styled.div`
   & textarea {
     width: 100%;
     height: 100%;
-    min-height: 100vh;
+    min-height: calc(100vh - 180px);
+
+    ${mq("lg")} {
+      min-height: calc(100vh - 110px);
+    }
   }
 `;
 
