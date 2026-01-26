b1ac9373f34058a2d2c5e4d87d232b6e5f4155f3
Luan Gjokaj <luan@riangle.com>
2026-01-26 17:01:47 +0100

    style: format

--- components/layout/Typography.ts
+++ components/layout/Typography.ts
@@ -13,38 +13,38 @@ import styled, { css } from "styled-components";
 
 const StyledHeading = css`
   font-weight: 900;
-`; 
+`;
 
-export const StyledH1 = styled.h1<{ theme: Theme }>` 
+export const StyledH1 = styled.h1<{ theme: Theme }>`
   ${StyledHeading};
   ${({ theme }) => styledH1(theme)}
-`; 
+`;
 
-export const StyledH2 = styled.h2<{ theme: Theme }>` 
+export const StyledH2 = styled.h2<{ theme: Theme }>`
   ${StyledHeading};
   ${({ theme }) => styledH2(theme)}
-`; 
+`;
 
-export const StyledH3 = styled.h3<{ theme: Theme }>` 
+export const StyledH3 = styled.h3<{ theme: Theme }>`
   ${StyledHeading};
   ${({ theme }) => styledH3(theme)}
-`; 
+`;
 
-export const StyledH4 = styled.h4<{ theme: Theme }>` 
+export const StyledH4 = styled.h4<{ theme: Theme }>`
   ${StyledHeading};
   ${({ theme }) => styledH4(theme)}
-`; 
+`;
 
-export const StyledH5 = styled.h5<{ theme: Theme }>` 
+export const StyledH5 = styled.h5<{ theme: Theme }>`
   ${StyledHeading};
   ${({ theme }) => styledH5(theme)}
-`; 
+`;
 
-export const StyledH6 = styled.h6<{ theme: Theme }>` 
+export const StyledH6 = styled.h6<{ theme: Theme }>`
   ${StyledHeading};
   ${({ theme }) => styledH6(theme)}
-`; 
+`;
 
-export const StyledText = styled.p<{ theme: Theme }>` 
+export const StyledText = styled.p<{ theme: Theme }>`
   ${({ theme }) => styledText(theme)}
-`; 
+`;
