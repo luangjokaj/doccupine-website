01ad8ff6a5eed6514c6df2229b3695389a3cda39
Luan Gjokaj <luan@riangle.com>
2026-01-26 16:36:03 +0100

    style: format

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,11 +6,11 @@ const content = `# 404 Not Found
 You just hit a route that doesn't exist.`;
 
 export const metadata: Metadata = {
-  title: 'Not Found',
-  description: 'Doccupine is a free and open-source document management system that allows you to store, organize, and share your documentation with ease.',
+  title: "Not Found",
+  description: "Doccupine is a free and open-source document management system that allows you to store, organize, and share your documentation with ease.",
   openGraph: {
-    title: 'Not Found',
-    description: 'Doccupine is a free and open-source document management system that allows you to store, organize, and share your documentation with ease.',
+    title: "Not Found",
+    description: "Doccupine is a free and open-source document management system that allows you to store, organize, and share your documentation with ease.",
   },
 };
 
